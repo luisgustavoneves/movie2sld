3ca117f3cd0f50f26db47b6c593adde60618c591
luisgustavoneves <luisgustavoneves@gmail.com>
2014-07-05 11:11:26 -0300

    performance improvement and slider

--- movie2sld.py
+++ movie2sld.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8
 import subprocess as sp
 import os
 import sys
 import shutil
+
 
 if sys.platform == 'win32':
     ffmpeg = '"C:\\Program Files\\ffmpeg-2.2.3-win64-static\\bin\\ffmpeg.exe"'
@@ -12,13 +14,17 @@ movie = sys.argv[1]
 ext = movie.split('.')[-1]
 name = movie[:-4]
 
+cmd_model = '%s -i "%s"' % (ffmpeg, movie)
+
 try:
     shutil.rmtree( name+ '_FILES')
 except:
     pass
 
 os.mkdir(name+ '_FILES')
-
+shutil.copy('jquery.js', name+ '_FILES')
+shutil.copy('unslider.min.js', name+ '_FILES')
+shutil.copy('style.css', name+ '_FILES')
 
 if len(sys.argv)>2:
     srt = sys.argv[2]
@@ -29,19 +35,51 @@ else:
 arq_srt = open(srt,'r')
 
 html = open(name+'.html', 'w')
-html.write('<html><body>\n')
+html.write('''<!doctype html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">	
+        <script src="%s_FILES/jquery.js"></script>
+        <script src="%s_FILES/unslider.min.js"></script>
+        <link rel="stylesheet" href="%s_FILES/style.css">
+        </head>
+    <body>
+        <div class="banner">
+            <ul>
+''' % (name, name, name) )
     
 text = arq_srt.readlines()
 i = 0
+cmds = []
+cmd = cmd_model
+
 for l in text:
     a = l.split('-->')
     if len(a)>1:
         a = a[0].split(',')
+        content = '<br><li><img height="auto" width="1000px" src="%s_FILES/%d.jpg"/>\n' % (name,i)
+        sp.call('%s -ss "%s" -i "%s" -vframes 1 "%s/%d.jpg"' % (ffmpeg, a[0], movie, name+'_FILES',  i) , shell=True) 
+        html.write(content)
         i = i+1
-        sp.call('%s -i "%s" -ss "%s" -vframes 1 "%s/%d.jpg"' % (ffmpeg, movie, a[0],name+'_FILES',  i) , shell=True) 
-        html.write('<img src ="%s_FILES/%d.jpg"/>\n' % (name,i))
     else:
         html.write('<h3>'+ l + '</h3>')
-        
-html.write('</body></html>')
+
+html.write('''
+            </div>
+            
+        <script>
+            $(function() {
+                $('.banner').unslider({
+                    speed: 0,               //  The speed to animate each slide (in milliseconds)
+                    delay: 30000,              //  The delay between slide animations (in milliseconds)
+                    complete: function() {},  //  A function that gets called after every slide animation
+                    keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+                    dots: true,               //  Display dot navigation
+                    fluid: false              //  Support responsive design. May break non-responsive designs
+                });
+            });
+        </script>
+    </body>
+</html>
+''')
 html.close()
